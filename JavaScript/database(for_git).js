87034f2244eb0001388bbb2edccf8fee2ddb6ddc
tagjune12 <tagjune12@gmail.com>
2022-02-14 04:03:37 +0900

    백업용

--- JavaScript/database(for_git).js
+++ JavaScript/database(for_git).js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
+import { getDatabase, ref, set, child, get, push } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,7 +9,13 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-d
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    //...
+    apiKey: "AIzaSyA5dBgP5uk1fRwStkXPlrP4740BoyXlofU",
+    authDomain: "todo-list-340309.firebaseapp.com",
+    projectId: "todo-list-340309",
+    storageBucket: "todo-list-340309.appspot.com",
+    messagingSenderId: "829367681458",
+    appId: "1:829367681458:web:cb27c9ecd622e912b224ab",
+    measurementId: "G-E75FMCK25H"
 };
 
 // Initialize Firebase
@@ -31,13 +37,92 @@ const loginEmail = (email, password) => {
 
 // 할일 조회 및 추가
 // 쓰기
-function writeData(userEmail, todoItem) {
+const updateData = (domain, id, todoItem) => {
     const db = getDatabase();
-    set(ref(db, 'users/' + userEmail), {
-        email: userEmail,
-        todoList: todoItem
+
+    // A post entry.
+    const postData = {
+        author: username,
+        uid: uid,
+        body: body,
+        title: title,
+        starCount: 0,
+        authorPic: picture
+    };
+
+    // Get a key for a new Post.
+    const newPostKey = push(child(ref(db), 'posts')).key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    const updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+    return update(ref(db), updates);
+}
+// 데이터를 추가함
+const writeData = (domain, id, todoItem) => {
+    const db = getDatabase();
+    // const todos = readData(domain)[id];
+
+    // console.log("writeData\n", todos);
+    // console.log("writeData2\n", todos[id]);
+
+    // set(push(ref(db, `${domain}/${id}/`)), {
+    //     id: todoItem
+    // });
+
+    readData(domain).then((snapshot) => {
+        if (snapshot.exists()) {
+            // 데이터가 있는경우
+            let todos = readData(domain)[id];
+            console.log("writeData1:", todos);
+        } else {
+            // 데이터가 없는 경우
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
     });
 }
 
+// 읽기
+const readData = (domain, id) => {
+    const dbRef = ref(getDatabase());
+    return get(child(dbRef, `${domain}`))
+}
 
-export { signupEmail, loginEmail };
+
+// const readData = (domain, id) => {
+//     const dbRef = ref(getDatabase());
+//     let result = null;
+//     get(child(dbRef, `${domain}`)).then((snapshot) => {
+//         if (snapshot.exists()) {
+//             // 데이터가 있는경우
+//             console.log(snapshot.val());
+//             result = snapshot.val();
+//         } else {
+//             // 데이터가 없는 경우
+//             console.log("No data available");
+//         }
+//     }).catch((error) => {
+//         console.error(error);
+//     });
+
+//     return result;
+// }
+
+const test = async (domain, id) => {
+    let result = undefined;
+
+    const dbRef = ref(getDatabase());
+    await get(child(dbRef, `${domain}`)).then((snapshot) => {
+        console.log("check");
+        result = snapshot.val();
+    }).catch((error) => {
+        console.log(error);
+    })
+
+    return result;
+}
+export { signupEmail, loginEmail, writeData, readData, test };
--- JavaScript/todolist.js
+++ JavaScript/todolist.js
@@ -1,54 +1,59 @@
-import { readData, writeData } from "./database.js";
+import { readData, writeData, test } from "./database.js";
 
 const userName = document.querySelector("#username");
 const todoInputForm = document.querySelector("#todo-input-form");
 const todoContent = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
-// 데이터베이스 활용X
-// const onTodoSubmit = (event) => {
-//     console.log("onTodoSubmit");
-//     event.preventDefault();
-
-//     const todoListItem = document.createElement("li");
-
-//     const todoText = document.createElement("span");
-//     todoText.innerText = todoContent.value;
-
-//     const checkBox = document.createElement("input")
-//     checkBox.type = "checkbox"
-
-//     const removeButton = document.createElement("button");
-//     removeButton.innerText = "❌";
-
-//     todoListItem.appendChild(checkBox);
-//     todoListItem.appendChild(todoText);
-//     todoListItem.appendChild(removeButton);
-
-//     todoList.appendChild(todoListItem);
-
-//     todoContent.value = "";
-// }
+let id = "";
+let domain = "";
 
 // 데이터베이스 활용O
-const onTodoSubmit = (event) => {
+const onSubmit = (event) => {
     console.log("onTodoSubmit");
     event.preventDefault();
 
-    // writeData()
+    // 서버에 데이터 쓰기
+    addTodo(todoContent.value);
+    // TodoList 갱신하기
 
     todoContent.value = "";
 }
 
-const getTodoList = (email) => {
-    const id = email.split("@")[0];
-    const domain = email.split("@")[1].split(".")[0];
+const getTodoList = (email = "") => {
+    console.log(email);
+    if (email !== "") {
+        const splittedEmail = email.split("@");
+        id = splittedEmail[0];
+        domain = splittedEmail[1].split(".")[0];
 
+    }
     userName.innerText = id;
     // 데이터 조회
-    readData(`${domain}/${id}`);
+    // readData(domain, id);
+
+    // readData(domain, id).then((snapshot) => {
+    //     if (snapshot.exists()) {
+    //         // 데이터가 있는경우
+    //         console.log(snapshot.val());
+    //     } else {
+    //         // 데이터가 없는 경우
+    //         console.log("No data available");
+    //     }
+    // }).catch((error) => {
+    //     console.error(error);
+    // });
+
+    // let testData = undefined;
+    console.log("getTodoList\n", test(domain, id, testData));
+    // console.log("testData: ", testData);
 }
 
-todoInputForm.addEventListener("submit", onTodoSubmit);
+const addTodo = (todo) => {
+    writeData(domain, id, todo);
+    getTodoList();
+}
+
+todoInputForm.addEventListener("submit", onSubmit);
 
 export { getTodoList };
