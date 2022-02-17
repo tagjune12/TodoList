c7907f801ddf9005165515b6721f65dc700130d6
tagjune12 <tagjune12@gmail.com>
2022-02-18 06:22:31 +0900

    CSS 및 로직 수정

--- JavaScript/logIn.js
+++ JavaScript/logIn.js
@@ -7,11 +7,8 @@ const loginPW = document.querySelector("#login-pw");
 const loginForm = document.querySelector("#login-form");
 const clockSection = document.querySelector("#clock-todolist");
 
-
 const tosignUpButton = document.querySelector("#signup");
-const signUpPage = document.querySelector("#signup-form");
-
-const HIDDEN_CLASS = "hidden";
+const signUpForm = document.querySelector("#signup-form");
 
 console.log("로그인 모듈 load");
 
@@ -21,37 +18,39 @@ const onSubmit = (event) => {
   const userEmail = loginID.value;
   const userPW = loginPW.value;
 
-  loginEmail(userEmail, userPW).then((userCredential) => {
-    const user = userCredential.user;
-    console.log(`signup result: ${userCredential}`);
-    alert("로그인 성공");
-    // 로그인폼 숨기기
-    loginForm.classList.add(HIDDEN_CLASS);
-    // 회원가입 버튼 숨기기
-    tosignUpButton.classList.add(HIDDEN_CLASS);
+  if (event.submitter.id === "login") {
+    loginEmail(userEmail, userPW).then((userCredential) => {
+      const user = userCredential.user;
+      console.log(`signup result: ${userCredential}`);
+      alert("로그인 성공");
 
-    // Todo List 페이지에 표시될 유저이름 표시
-    // const userName = document.querySelector("#username");
-    // userName.innerText = `${userEmail.split("@")[0]}'s today Todo List`;
-    title.innerText = `Hello ${userEmail.split("@")[0]}!`;
+      // 로그인폼 숨기기
+      loginForm.style.display = "none";
 
-    // Todo List 표시
-    showTodoList(userEmail);
-    // TodoList와 시계 표시
-    clockSection.classList.remove(HIDDEN_CLASS);
+      // 회원가입폼 숨기기
+      signUpForm.style.display = "none";
 
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // console.log(errorCode);
-    console.log(errorMessage);
-    alert(errorMessage);
-  })
+      // Todo List 페이지에 표시될 유저이름 표시
+      title.innerText = `Hello ${userEmail.split("@")[0]}!`;
+
+      // TodoList와 시계 표시
+      clockSection.style.display = "flex";
+
+      // Todo List 표시
+      showTodoList(userEmail);
+
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+    })
+  }
 }
 
 const onSignUpClick = () => {
-  loginForm.classList.add(HIDDEN_CLASS);
-  signUpPage.classList.remove(HIDDEN_CLASS);
+  loginForm.style.display = "none";
+  signUpForm.style.display = "flex";
   console.log("회원가입 버튼 클릭");
 }
 
